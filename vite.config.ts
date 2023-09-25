d97c3853ee1d14c8d266b492fcbd886008d8ff32
Paul Vega <paulmax951@gmail.com>
2023-09-25 12:18:33 -0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: ['**/*.svg'],
   plugins: [react()],
+  base: 'https://pvega95/github.io/make-front'
 })
